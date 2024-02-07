7c142e153f1dd2f9c68ec222e58ac72b45777f74
rerebeccajiang <118489430+rerebeccajiang@users.noreply.github.com>
2024-02-07 21:18:44 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- my-folder/problems/contains_duplicate/solution.py
+++ my-folder/problems/contains_duplicate/solution.py
@@ -4,9 +4,17 @@ class Solution:
         # return a sorted list
         # for range(len(sorted list)) in list, if i = i+1 return true, else false
 
-        sortedList = sorted(nums)
-        for i in range(len(sortedList)-1):
-            if sortedList[i] == sortedList[i+1]:
+        # sortedList = sorted(nums)
+        # for i in range(len(sortedList)-1):
+        #     if sortedList[i] == sortedList[i+1]:
+        #         return True
+        #     if i == len(sortedList) - 2:
+        #         return False
+
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
                 return True
-            if i == len(sortedList) - 2:
-                return False
+            hashset.add(i)
+        return False
