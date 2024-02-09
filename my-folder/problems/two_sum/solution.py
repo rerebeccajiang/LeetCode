4bb9f52db0f19e03aaf643b0e9e94befb226a9fa
rerebeccajiang <118489430+rerebeccajiang@users.noreply.github.com>
2024-02-09 20:53:22 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,14 +1,26 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dict = {}
-        for i in range(0, len(nums)):
-            compliment = target - nums[i]
-            if compliment in dict.keys():
-                return[i, dict.get(compliment)]
-            else:
-                dict[nums[i]] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # stor = defaultdict(list)
+
+        # for i in range(len(nums)):
+        #     stor[nums[i]].append(i)
+        # print(stor)
+
+        # for i in range(len(nums)):
+        #     complement = stor.get(target - nums[i])
+        #     if complement != None:
+        #         if len(complement) == 2:
+        #             return complement
+        #         elif target - nums[i] == nums[i]:
+        #             continue
+        #         else:
+        #             complement.append(i)
+        #             return complement
+        
+        preMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in preMap:
+                return [preMap[diff], i]
+            preMap[n] = i
+        
