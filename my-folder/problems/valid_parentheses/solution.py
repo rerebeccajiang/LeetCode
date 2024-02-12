75f72507ef37db864d1fdb8cecb93f553e12fa79
rerebeccajiang <118489430+rerebeccajiang@users.noreply.github.com>
2024-02-12 17:46:24 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- my-folder/problems/valid_parentheses/solution.py
+++ my-folder/problems/valid_parentheses/solution.py
@@ -1,24 +1,39 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        map = {
-            ")" : "(",
-            "}" : "{",
-            "]" : "["
-        }
-        
+        # openBr = ["(", "[", "{"]
+        # closeBr = [")", "]", "}"]
+
+        # stack = []
+        # for i in s:
+        #     if len(stack)==0:
+        #         stack.append(i)
+
+        #     elif i in openBr : # in open bracket
+        #         stack.append(i)
+
+        #     else: # in closing bracket
+        #         bracket = stack.pop()
+        #         if bracket in closeBr or closeBr[openBr.index(bracket)] != i:
+        #             return False
+                    
+
+        # return len(stack) == 0
+
         stack = []
-        
+        closeToOpen = {"]": "[", "}": "{",")":"(" }
+
         for i in s:
-            if i in map:
-                if stack and stack[-1] == map[i]:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
                     stack.pop()
                 else:
                     return False
-            
             else:
                 stack.append(i)
+
+        return len(stack)==0
+
         
-        return True if not stack else False
-                
-                
+
+
+        
